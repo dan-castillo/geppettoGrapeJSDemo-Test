9f45340bad2a7298d29b4067ebce94725cd05871
DHINAKAR-P <dhinakarintech@gmail.com>
2020-01-09 19:38:22 +0530

    traits code added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GrapejsComponent } from './grapejs/grapejs.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GrapejsComponent,
+    TutorialComponent
   ],
   imports: [
     BrowserModule,
