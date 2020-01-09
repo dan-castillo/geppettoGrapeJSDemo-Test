b4bcc5f52a9cd7e72e409e3b288a2c97084df0fc
DHINAKAR-P <dhinakarintech@gmail.com>
2020-01-09 19:05:40 +0530

    GrapesJs - Blocks added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GrapejsComponent } from './grapejs/grapejs.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'grapesJs',
+    component: GrapejsComponent
+  },
+  {
+    path: '',
+    component: TutorialComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
